669eda11e434906e9935b924bdd8374907329110
Ryan Townsend <ryan@thinkrefresh.com>
2011-04-11 10:54:16 +0100

    Moved routing to helpers

---
+++ app/helpers/nested_db_routing_helper.rb
@@ -0,0 +1,17 @@
+module NestedDbRoutingHelper
+  def taxonomy_relative_to_instance_path
+    request.path.gsub(/\/instances\/.*?$/, '')
+  end
+  
+  def instance_relative_to_taxonomy_path(instance, action = nil)
+    path = "#{instances_relative_to_taxonomy_path}/#{instance.id}"
+    path += "/#{action}" if action
+    path
+  end
+  
+  def instances_relative_to_taxonomy_path(action = nil)
+    path  = request.path.gsub(/(\/taxonomies\/\w{24}).*?$/, "\1/instances")
+    path += "/#{action}" if action
+    path
+  end
+end
