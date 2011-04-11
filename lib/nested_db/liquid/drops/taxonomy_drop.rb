6656c1d931a049e9830e599c6ba0cbb1af8cc165
Ryan Townsend <ryan@thinkrefresh.com>
2011-04-11 15:16:27 +0100

    Added find_by_ methods for taxonomy drop

--- lib/nested_db/liquid/drops/taxonomy_drop.rb
+++ lib/nested_db/liquid/drops/taxonomy_drop.rb
@@ -1,25 +1,35 @@
 module NestedDb
   class TaxonomyDrop < ::Liquid::Drop
+    attr_accessor :taxonomy
+    
     def initialize(taxonomy)
-      @taxonomy = taxonomy
+      self.taxonomy = taxonomy
+      
+      taxonomy.property_fields.each { |f|
+        self.class.send(:define_method, "find_by_#{f.name.to_s}") do |value|
+          taxonomy.instances.where({ f.name => value }).limit(100).map { |i|
+            InstanceDrop.new(i, self)
+          }
+        end
+      }
     end
   
     def name
-      @taxonomy.name
+      taxonomy.name
     end
   
     def all
-      @taxonomy.instances.limit(100).map { |i|
+      taxonomy.instances.limit(100).map { |i|
         InstanceDrop.new(i, self)
       }
     end
     
     def count
-      @taxonomy.instances.count
+      taxonomy.instances.count
     end
   
     def fields
-      @taxonomy.instances.build.fields.keys
+      taxonomy.instances.build.fields.keys
     end
   end
 end
