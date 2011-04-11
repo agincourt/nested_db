07cb80f6aad4d162e50f80c5fcf0f34783886811
Ryan Townsend <ryan@thinkrefresh.com>
2011-04-11 11:32:34 +0100

    Updated liquid includes

---
+++ lib/nested_db/liquid.rb
@@ -0,0 +1,3 @@
+require 'nested_db/liquid/drops/instance_drop'
+require 'nested_db/liquid/drops/taxonomies_drop'
+require 'nested_db/liquid/drops/taxonomy_drop'
---
+++ lib/nested_db/liquid/drops/taxonomy_drop.rb
@@ -0,0 +1,21 @@
+module NestedDb
+  class TaxonomyDrop < Liquid::Drop
+    def initialize(taxonomy)
+      @taxonomy = taxonomy
+    end
+  
+    def name
+      @taxonomy.name
+    end
+  
+    def all_instances
+      @taxonomy.instances.limit(100).map { |i|
+        InstanceDrop.new(i)
+      }
+    end
+  
+    def fields
+      @taxonomy.instances.build.fields.keys
+    end
+  end
+end
