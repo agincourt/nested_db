4614df52cae7566b7c93384abed793c599886121
Ryan Townsend <ryan@thinkrefresh.com>
2011-04-11 15:25:38 +0100

    Updated finders

--- lib/nested_db/liquid/drops/taxonomy_drop.rb
+++ lib/nested_db/liquid/drops/taxonomy_drop.rb
@@ -6,10 +6,15 @@ module NestedDb
       self.taxonomy = taxonomy
       
       taxonomy.property_fields.each { |k,v|
-        self.class.send(:define_method, "find_by_#{k.to_s}") do |value|
+        self.class.send(:define_method, "find_all_by_#{k.to_s}") do |value|
           taxonomy.instances.where({ k => value }).limit(100).map { |i|
             InstanceDrop.new(i, self)
           }
+        end
+        
+        self.class.send(:define_method, "find_one_by_#{k.to_s}") do |value|
+          instance = taxonomy.instances.where({ k => value }).find(:first)
+          InstanceDrop.new(instance, self) if instance
         end
       }
     end
