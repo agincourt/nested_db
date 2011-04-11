c77f5b759c0c02f161b21a3ac7bc3232b94b61ba
Ryan Townsend <ryan@thinkrefresh.com>
2011-04-11 15:58:52 +0100

    Remove unneeded methods

--- lib/nested_db/liquid/drops/taxonomy_drop.rb
+++ lib/nested_db/liquid/drops/taxonomy_drop.rb
@@ -4,19 +4,6 @@ module NestedDb
     
     def initialize(taxonomy)
       self.taxonomy = taxonomy
-      
-      taxonomy.property_fields.each { |k,v|
-        self.class.send(:define_method, "find_all_by_#{k.to_s}") do |value|
-          taxonomy.instances.where({ k => value }).limit(100).map { |i|
-            InstanceDrop.new(i, self)
-          }
-        end
-        
-        self.class.send(:define_method, "find_one_by_#{k.to_s}") do |value|
-          instance = taxonomy.instances.where({ k => value }).find(:first)
-          InstanceDrop.new(instance, self) if instance
-        end
-      }
     end
   
     def name
