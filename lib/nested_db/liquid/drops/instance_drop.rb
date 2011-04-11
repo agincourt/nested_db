b77efea6d335a99167ca1f2b2fd4df6d7d069a28
Ryan Townsend <ryan@thinkrefresh.com>
2011-04-11 16:34:45 +0100

    Update attempt [2]

--- lib/nested_db/liquid/drops/instance_drop.rb
+++ lib/nested_db/liquid/drops/instance_drop.rb
@@ -1,26 +1,26 @@
 module NestedDb
   class InstanceDrop < ::Liquid::Drop
-    attr_accessor :instance
+    attr_accessor :taxonomy_instance
     attr_accessor :taxonomy_drop
     
     def initialize(instance, taxonomy_drop = nil)
-      self.instance      = instance
-      self.taxonomy_drop = taxonomy_drop if taxonomy_drop
+      self.taxonomy_instance = instance
+      self.taxonomy_drop     = taxonomy_drop if taxonomy_drop
     
       # loop through fields
-      self.instance.fields.keys.each { |k|
+      taxonomy_instance.fields.keys.each { |k|
         self.class.send(:define_method, k.to_sym) do
-          self.instance.read_attribute(k)
+          taxonomy_instance.read_attribute(k)
         end                                                                                                                                                                        
       }
     end
     
     def fields
-      instance.fields.keys
+      taxonomy_instance.fields.keys
     end
     
     def taxonomy
-      self.taxonomy_drop ||= TaxonomyDrop.new(instance.taxonomy)
+      self.taxonomy_drop ||= TaxonomyDrop.new(taxonomy_instance.taxonomy)
     end
   end
 end
