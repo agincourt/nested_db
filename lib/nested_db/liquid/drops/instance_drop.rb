bee86e7faa3c9923669d128d99201ca7e2ffa858
Ryan Townsend <ryan@thinkrefresh.com>
2011-04-12 11:59:14 +0100

    Removed to_liquid reference

--- lib/nested_db/liquid/drops/instance_drop.rb
+++ lib/nested_db/liquid/drops/instance_drop.rb
@@ -1,13 +1,13 @@
 module NestedDb
   class InstanceDrop < ::Liquid::Drop
-    attr_accessor :taxonomy_instance
+    attr_accessor :instance
     attr_accessor :taxonomy_drop
     
-    delegate :read_attribute, :to => "taxonomy_instance"
+    delegate :read_attribute, :to => "instance"
     
     def initialize(instance, taxonomy_drop = nil)
-      self.taxonomy_instance = instance
-      self.taxonomy_drop     = taxonomy_drop if taxonomy_drop
+      self.instance      = instance
+      self.taxonomy_drop = taxonomy_drop if taxonomy_drop
     
       # loop through fields
       fields.each do |k|
@@ -18,18 +18,11 @@ module NestedDb
     end
     
     def fields
-      taxonomy_instance.fields.keys
+      instance.fields.keys
     end
     
     def taxonomy
-      self.taxonomy_drop ||= TaxonomyDrop.new(taxonomy_instance.taxonomy)
-    end
-    
-    def to_liquid
-      {
-        'fields'   => fields,
-        'taxonomy' => taxonomy
-      }.merge(fields.inject({}) { |hash, f| hash.merge!(f => read_attribute(f)) })
+      self.taxonomy_drop ||= TaxonomyDrop.new(instance.taxonomy)
     end
   end
 end
