909318d0dffaeab79f9e2cd8f5c7089b1fbf6d7c
Ryan Townsend <ryan@thinkrefresh.com>
2011-04-12 13:08:04 +0100

    Added error handling

---
+++ lib/nested_db/liquid/filters/errors.rb
@@ -0,0 +1,10 @@
+module ErrorFilter
+  def die(input, status_code, status_message = nil)
+    # if input is nil/false
+    unless input
+      raise StandardError, "Fail: #{status_code}, #{status_message}"
+    end
+  end
+end
+
+Liquid::Template.register_filter(ErrorFilter)
