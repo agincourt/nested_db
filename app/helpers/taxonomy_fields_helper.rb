c7cbd7db27da71fc7eb203b2f991372b31248564
Ryan Townsend <ryan@thinkrefresh.com>
2011-04-26 09:37:07 +0100

    Updated helper

--- app/helpers/taxonomy_fields_helper.rb
+++ app/helpers/taxonomy_fields_helper.rb
@@ -12,7 +12,14 @@ module TaxonomyFieldsHelper
     when 'plain_text'
       builder.text_area property.name, :class => 'plain'
     when 'file'
-      builder.file_field property.name, :class => 'file'
+      if builder.object.new_record?
+        builder.file_field property.name, :class => 'file'
+      else
+        [
+          builder.file_field(property.name, :class => 'file'),
+          image_tag( NestedDb::Instance.image_variation(builder.object.send(property.name), 'thumbnail'), :size => '95x70', :alt => 'Preview Image' )
+        ].join.html_safe
+      end
     else
       builder.text_field property.name, :class => "text data_type_#{property.data_type}"
     end
