89f1a83eab39f2835d4a0f473d77b237d5aef7f5
Ryan Townsend <ryan@thinkrefresh.com>
2011-05-03 14:40:23 +0100

    Updated callback

--- lib/mongoid/ext.rb
+++ lib/mongoid/ext.rb
@@ -3,12 +3,12 @@ module Mongoid #:nodoc:
   module Relations #:nodoc:
     class Many < Proxy
       # simply processes a callback
-      def build_callback
+      def build_with_callback
         run_callbacks(:build) { self }
       end
       
       # call build_callback after build
-      alias_method_chain :build, :build_callback
+      alias_method_chain :build, :callback
       
       define_model_callbacks :build, :only => :after
     end
