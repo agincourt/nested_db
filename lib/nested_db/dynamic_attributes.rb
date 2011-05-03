fc70d81034572e9c94a319fa964676741c5baa28
Ryan Townsend <ryan@thinkrefresh.com>
2011-05-03 15:52:20 +0100

    Added after_initialize callback

--- lib/nested_db/dynamic_attributes.rb
+++ lib/nested_db/dynamic_attributes.rb
@@ -3,7 +3,9 @@ module NestedDb
     def self.included(base)
       base.send(:include, InstanceMethods)
       base.class_eval do
-        after_build :extend_based_on_taxonomy
+        after_build      :extend_based_on_taxonomy
+        after_initialize :extend_based_on_taxonomy,
+          :if => proc { |obj| obj.taxonomy }
       end
     end
     
