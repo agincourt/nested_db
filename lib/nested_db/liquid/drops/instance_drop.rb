a2c8481ea419ec8f70e13547b2eb9f51a9ef7bd9
Ryan Townsend <ryan@thinkrefresh.com>
2011-05-16 13:22:57 +0100

    Updated instance_drop to include relations

--- lib/nested_db/liquid/drops/instance_drop.rb
+++ lib/nested_db/liquid/drops/instance_drop.rb
@@ -6,25 +6,44 @@ module NestedDb
     delegate :read_attribute,     :to => "instance"
     delegate :attribute_present?, :to => "instance"
     delegate :attributes,         :to => "instance"
+    delegate :auto_increment_id,  :to => "instance"
+    delegate :created_at,         :to => "instance"
+    delegate :updated_at,         :to => "instance"
     
     def initialize(instance, taxonomy_drop = nil)
       self.instance      = instance
       self.taxonomy_drop = taxonomy_drop if taxonomy_drop
+    end
     
-      # loop through fields
-      fields.each do |k|
-        self.class.send(:define_method, k.to_sym) do
-          attribute_present?("#{k.to_s}_rich_text_processed") ? read_attribute("#{k.to_s}_rich_text_processed") : read_attribute(k)
-        end                                                                                                                                                                        
+    def to_liquid
+      # memoize
+      return @properties if defined?(@properties)
+      # load fields from taxonomy properties
+      @properties = taxonomy.properties.inject({}) do |result,property|
+        value = case property.data_type
+        when 'rich_text'
+          read_attribute("#{property.name}_rich_text_processed")
+        when 'belongs_to'
+          InstanceDrop.new(read_attribute(property.name))
+        when 'has_many'
+          read_attribute(property.name).map { |i| InstanceDrop.new(i) }
+        else
+          read_attribute(property.name)
+        end
+        
+        result.merge(property.name => value)
       end
-    end
-    
-    def fields
-      instance.attributes.keys
-    end
-    
-    def taxonomy
-      self.taxonomy_drop ||= TaxonomyDrop.new(instance.taxonomy)
+      
+      # merge some other properties
+      @properties.merge!({
+        :taxonomy   => taxonomy_drop || TaxonomyDrop.new(instance.taxonomy),
+        :id         => auto_increment_id,
+        :created_at => created_at,
+        :updated_at => updated_at
+      })
+      
+      # return the properties
+      @properties
     end
   end
 end
