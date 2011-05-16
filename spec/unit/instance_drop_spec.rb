d98066d2d1fa332317893c57d3d8abc9b740e1df
Ryan Townsend <ryan@thinkrefresh.com>
2011-05-16 14:25:01 +0100

    Tested belongs_to/has_many relations

--- spec/unit/instance_drop_spec.rb
+++ spec/unit/instance_drop_spec.rb
@@ -11,13 +11,37 @@ describe NestedDb::InstanceDrop do
         :name      => 'Category',
         :reference => 'categories'
       })
-      # add a file property
+      # add a normal property
       @taxonomy.physical_properties.create!({
         :name      => 'name',
         :data_type => 'string',
         :required  => true
       })
-      # return
+      # create the second taxonomy to relate to
+      @taxonomy_two = NestedDb::Taxonomy.create!({
+        :name      => 'Article',
+        :reference => 'articles'
+      })
+      # add a normal property to articles
+      @taxonomy_two.physical_properties.create!({
+        :name      => 'name',
+        :data_type => 'string',
+        :required  => true
+      })
+      # add a belongs relation from taxonomy two to taxonomy one
+      @taxonomy_two.physical_properties.create!({
+        :name                 => 'category',
+        :data_type            => 'belongs_to',
+        :association_property => 'name',      # just used for display
+        :association_taxonomy => 'categories' # based on reference
+      })
+      # add a has_many relation from taxonomy one to taxonomy two
+      @taxonomy.physical_properties.create!({
+        :name                 => 'articles',
+        :data_type            => 'has_many',
+        :association_property => 'category', # used for relation
+        :association_taxonomy => 'articles'  # based on reference
+      })
       @taxonomy
     end
 
@@ -34,6 +58,23 @@ describe NestedDb::InstanceDrop do
       (inst.taxonomy.properties.keys + ['id', 'taxonomy', 'created_at', 'updated_at']).each do |key|
         drop.to_liquid.keys.should include(key)
       end
+    end
+    
+    it "should load the associations from the taxonomy" do
+      inst = instance
+      inst.update_attributes({
+        :articles_attributes => { '0' => {
+          'name' => 'Test 2'
+        } }
+      })
+      inst.articles.size.should == 1
+      inst.articles.first.persisted?.should == true
+      inst.articles.first.category.should == inst
+      drop = NestedDb::InstanceDrop.new(inst)
+      drop.to_liquid.keys.should include('articles')
+      drop.to_liquid['articles'].size.should     == 1
+      drop.to_liquid['articles'][0].class.should == NestedDb::InstanceDrop
+      drop.to_liquid['articles'][0].to_liquid['category'].instance.should == drop.instance
     end
     
     it "should return a drop for it's taxonomy" do
