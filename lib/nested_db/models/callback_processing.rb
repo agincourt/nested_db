955f3b345b6cd3c9cd817a08b30c352d4cd7ac8a
Ryan Townsend <ryan@thinkrefresh.com>
2011-05-25 15:33:02 +0100

    Updated method naming

--- lib/nested_db/models/callback_processing.rb
+++ lib/nested_db/models/callback_processing.rb
@@ -4,38 +4,38 @@ module NestedDb
       def self.included(base)
         base.send(:include, InstanceMethods)
         base.class_eval do
-          before_create :run_before_create_callbacks
-          after_create  :run_after_create_callbacks
-          before_update :run_before_update_callbacks
-          after_update  :run_after_update_callbacks
-          before_save   :run_before_save_callbacks
-          after_save    :run_after_save_callbacks
+          before_create :run_taxonomy_before_create_callbacks
+          after_create  :run_taxonomy_after_create_callbacks
+          before_update :run_taxonomy_before_update_callbacks
+          after_update  :run_taxonomy_after_update_callbacks
+          before_save   :run_taxonomy_before_save_callbacks
+          after_save    :run_taxonomy_after_save_callbacks
         end
       end
       
       module InstanceMethods
         private
-        def run_before_create_callbacks
+        def run_taxonomy_before_create_callbacks
           run_taxonomy_callbacks :before_create
         end
         
-        def run_after_create_callbacks
+        def run_taxonomy_after_create_callbacks
           run_taxonomy_callbacks :after_create
         end
         
-        def run_before_update_callbacks
+        def run_taxonomy_before_update_callbacks
           run_taxonomy_callbacks :before_update
         end
         
-        def run_after_update_callbacks
+        def run_taxonomy_after_update_callbacks
           run_taxonomy_callbacks :after_update
         end
         
-        def run_before_save_callbacks
+        def run_taxonomy_before_save_callbacks
           run_taxonomy_callbacks :before_save
         end
         
-        def run_after_save_callbacks
+        def run_taxonomy_after_save_callbacks
           run_taxonomy_callbacks :after_save
         end
         
