23e3e0916ee005e207b976d6e0b76f711c787c06
Ryan Townsend <ryan@thinkrefresh.com>
2011-06-02 13:41:44 +0100

    Enforce integer indexes

--- lib/nested_db/models/property.rb
+++ lib/nested_db/models/property.rb
@@ -13,19 +13,23 @@ module NestedDb
           scope :ordered, order_by(:index.asc, :name.asc)
           
           # validation
-          validates_presence_of   :name
-          validates_uniqueness_of :name,
+          validates_presence_of     :name
+          validates_uniqueness_of   :name,
             :scope => :taxonomy
-          validates_format_of     :name,
+          validates_format_of       :name,
             :with    => /^[a-z0-9\_]*$/i,
             :message => 'can only contain letters, digits, hyphons and underscores'
-          validates_format_of     :name,
+          validates_format_of       :name,
             :with    => /^[a-z]+/i,
             :message => 'must start with an alphabetic letter'
-          validates_presence_of   :data_type
+          validates_presence_of     :data_type
+          validates_numericality_of :index,
+            :only_integer             => true,
+            :greater_than_or_equal_to => 0
           
           # callbacks
           before_validation :downcase_name
+          before_validation :set_index
         end
         base.send(:include, InstanceMethods)
       end
@@ -42,6 +46,11 @@ module NestedDb
         private  
         def downcase_name
           self.name.try(:downcase!)
+        end
+        
+        def set_index
+          self.index ||= 9999
+          self.index = [[0, self.index.to_i].max, 9999].min
         end
       end
     end
